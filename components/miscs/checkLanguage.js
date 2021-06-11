06bf8d405bc3708cfc656007972fbeb29c64a7de
nyambayarsaint <nyambayar.saint@gmail.com>
2021-06-11 11:35:57 +0800

    save

--- components/miscs/checkLanguage.js
+++ components/miscs/checkLanguage.js
@@ -6,12 +6,12 @@ const checkLanguage = async (queryString, server) => {
     // const httpObject = { method: 'post', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ query: `query ${queryString}` }) }
     return new Promise(async (resolve, reject) => {
         if (cookies['tblang'] === "en") {
-            const res = await fetch(process.env.serverUrlEnglish + queryString);
+            const res = await fetch(process.env.serverUrlEnglish + queryString, {mode: "no-cors"});
             const pageData = await res.json();
             return resolve({ data: pageData });
         }
         else {
-            const res = await fetch(process.env.serverUrl + queryString);
+            const res = await fetch(process.env.serverUrl + queryString, {mode: "no-cors"});
             const pageData = await res.json();
             return resolve({ data: pageData });
         }
