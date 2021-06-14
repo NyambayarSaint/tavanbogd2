d0a2b13aa73bb2fde33e6d12010e3dc92772d00d
nyambayarsaint <nyambayar.saint@gmail.com>
2021-06-14 17:45:40 +0800

    save

--- components/dynamic/Landingpage.js
+++ components/dynamic/Landingpage.js
@@ -1,60 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import minimize from '../miscs/minimize';
-import Particles from 'react-particles-js';
 import { MenuContext } from '../miscs/ContextMenuProvider';
 
 const Landingpage = ({ data }) => {
     const { general } = React.useContext(MenuContext);
     return (
         <Container>
-            <Particles
-                width={'100vw'}
-                height={'100vh'}
-                params={{
-                    "particles": {
-                        "number": {
-                            "value": 100,
-                            "density": {
-                                "enable": true,
-                                "value_area": 1500
-                            }
-                        },
-                        "color": 'rgba(0,0,0,0.2)',
-                        "line_linked": {
-                            "enable": true,
-                            "opacity": 0.02
-                        },
-                        "move": {
-                            "direction": "right",
-                            "speed": 0.05
-                        },
-                        "size": {
-                            "value": 1
-                        },
-                        "opacity": {
-                            "anim": {
-                                "enable": true,
-                                "speed": 1,
-                                "opacity_min": 0.05
-                            }
-                        }
-                    },
-                    "interactivity": {
-                        "events": {
-                            "onclick": {
-                                "enable": true,
-                                "mode": "push"
-                            }
-                        },
-                        "modes": {
-                            "push": {
-                                "particles_nb": 1
-                            }
-                        }
-                    },
-                    "retina_detect": true
-                }} />
             <div className="con">
                 <div className="top">
                     <img src={minimize(data.Logo)} />
