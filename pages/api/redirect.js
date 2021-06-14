64e0d6b1c47532d1fa451cb3048d17d00ee47f89
nyambayarsaint <nyambayar.saint@gmail.com>
2021-06-14 12:02:13 +0800

    save

--- pages/api/redirect.js
+++ pages/api/redirect.js
@@ -5,8 +5,9 @@ export default async (req, res) => {
     const { url } = req.body
 
     if (req.method === "POST") {
-
+        console.log('Came!');
         let responseFromStrapi = await Axios(url);
+        console.log(responseFromStrapi.data,'dataaaaa');
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         return res.end(JSON.stringify({ data: responseFromStrapi.data }));
