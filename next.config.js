f7f83f0fbef92e04bbbd8f554df29473f142343e
nyambayarsaint <nyambayar.saint@gmail.com>
2021-06-23 11:43:12 +0800

    save

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
         serverUrl: 'https://admin-mn.tavanbogd.com',
         serverUrlEnglish: 'https://admin-en.tavanbogd.com/',
-        frontUrl: 'http://tavanbogd.com',
+        frontUrl: 'https://tavanbogd.com',
         pageUrl: '/p'
     }
 }
